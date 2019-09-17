d6dbca275681971f6c135946e600680526f01547
ahmettuztasi <tuztasiahmet@gmail.com>
2019-09-17 11:15:31 -0700

    Initial Commit

---
+++ TwitterAppSB/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TwitterAppSB
+//
+//  Created by KO on 17/09/2019.
+//  Copyright © 2019 Ahmet Tuztașı. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TwitterAppSB/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TwitterAppSB
+//
+//  Created by KO on 17/09/2019.
+//  Copyright © 2019 Ahmet Tuztașı. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
